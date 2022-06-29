5b224f7639ee1f3bf8411fe0e1859b7c5ef32b80
Marius115 <cascaranomeister@gmail.com>
2022-06-29 23:07:02 +0000

    :rage1: Tabla lista

--- src/models.py
+++ src/models.py
@@ -6,25 +6,52 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
+
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    #login = Column(String(250), nullable=False)
+    #preguntar por el login
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorite(Base):
+    __tablename__ = 'favorite'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    planet_id = Column(Integer, ForeignKey("planet.id"))
+    character_id = Column(Integer, ForeignKey("character.id"))
+
+class Character(Base):
+    __tablename__ = 'character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    uid = Column(Integer, nullable=False)
+    name = Column(String(150), nullable=False)
+    gender = Column(String(150), nullable=False)
+    bod = Column(String(150), nullable=False)
+    height = Column(Integer)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    uid = Column(Integer, nullable=False)
+    name = Column(String(150), nullable=False)
+    terrain = Column(String(150), nullable=False)
+    climate = Column(String(150), nullable=False)
+    population = Column(Integer)
+    
 
     def to_dict(self):
         return {}
